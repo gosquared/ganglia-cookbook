74852f43a5656a45be75b9a9d37580d19d0e1d6e
Gerhard Lazu <gerhard@lazu.co.uk>
2011-10-24 16:33:19 +0100

    Added files that are missing from the default webfrontend

--- recipes/web.rb
+++ recipes/web.rb
@@ -25,5 +25,21 @@ directory "/etc/ganglia-webfrontend"
 service "apache2" do
   service_name "httpd" if platform?( "redhat", "centos", "fedora" )
   supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+  action [:enable, :start]
+end
+
+%w[cluster_legend node_legend].each do |filename|
+  cookbook_file "/usr/share/ganglia-webfrontend/#{filename}.html" do
+    cookbook "ganglia"
+    source "#{filename}.html"
+    owner "root"
+    group "root"
+    mode "0644"
+  end
+end
+
+apache2_passwd "Ganglia user" do
+  username node[:ganglia][:admin][:user]
+  password node[:ganglia][:admin][:password]
+  action :add
 end
