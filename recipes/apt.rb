9af92c22ce74469949567614a2d7d35e9960a2a9
Gerhard Lazu <gerhard@lazu.co.uk>
2011-11-16 18:09:22 +0000

    This is now the default Conflicts:
      recipes/apt.rb

--- recipes/apt.rb
+++ recipes/apt.rb
@@ -1,7 +1,5 @@
 apt_repository "ganglia" do
   uri "http://ppa.launchpad.net/rufustfirefly/ganglia/ubuntu"
-  distribution `lsb_release -cs`.chomp
-  components ["main"]
   keyserver "pgpkeys.mit.edu"
   key "A93EFBE2"
   action :add
