bba0413422dc2f5136c3483acd206bf32d65dca9
Gerhard Lazu <gerhard@lazu.co.uk>
2011-12-07 13:00:54 +0000

    wan_up aware
    Graceful skipping if wan_up? is not present

--- recipes/python_modules.rb
+++ recipes/python_modules.rb
@@ -1,23 +1,31 @@
-git "/usr/lib/ganglia/python_modules_available" do
-  repository "git://github.com/gchef/gmond_python_modules.git"
-  reference "master"
-  action :sync
+if Chef::Util.respond_to?(:wan_up?)
+  skip = Chef::Util.wan_up? ? false : true
+else
+  skip = false
 end
 
-directory "/usr/lib/ganglia/python_modules_enabled"
+if !skip
+  git "/usr/lib/ganglia/python_modules_available" do
+    repository "git://github.com/gchef/gmond_python_modules.git"
+    reference "master"
+    action :sync
+  end
 
-template "/etc/ganglia/conf.d/modpython.conf" do
-  cookbook "ganglia"
-  source "modpython.conf.erb"
-  notifies :restart, resources(:service => "ganglia-monitor")
-end
+  directory "/usr/lib/ganglia/python_modules_enabled"
 
-node[:ganglia][:python_modules][:enabled].each do |python_module|
-  ganglia_python_module python_module
-end
+  template "/etc/ganglia/conf.d/modpython.conf" do
+    cookbook "ganglia"
+    source "modpython.conf.erb"
+    notifies :restart, resources(:service => "ganglia-monitor")
+  end
 
-node[:ganglia][:python_modules][:disabled].each do |python_module|
-  ganglia_python_module python_module do
-    disable true
+  node[:ganglia][:python_modules][:enabled].each do |python_module|
+    ganglia_python_module python_module
+  end
+
+  node[:ganglia][:python_modules][:disabled].each do |python_module|
+    ganglia_python_module python_module do
+      disable true
+    end
   end
 end
